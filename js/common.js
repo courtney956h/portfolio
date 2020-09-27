a82fa0deaffedc96c7d0f1c2d28355e8370e846f
courtney956h <68522358+courtney956h@users.noreply.github.com>
2020-09-27 17:39:24 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,28 @@
+(function($){
+    sct = $(this).scrollTop();
+    // h1 클릭시 이벤트
+    $('.main_s1_p1 > h1 > a').on('click', function(){
+        $('.main_s1_p1').stop().animate({
+            width: '100%'
+        }, 1000)
+    })
+
+
+    // 마우스 휠
+    $('.section').on('mousewheel', function(e, wh){
+        prev = $(this).prev().offset().top
+        next = $(this).next().offset().top
+        if ( wh > 0 ) {
+           $('html, body').stop().animate({
+               scrollTop: prev
+           }, 800, 'linear')
+        } else if ( wh < 0 ) {
+            $('html, body').stop().animate({
+                scrollTop: next
+            }, 800, 'linear')
+        }
+
+    })
+
+
+})(jQuery)
