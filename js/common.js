fce54a6dd43cf49d4d47ae5062739802a45e2612
courtney956h <68522358+courtney956h@users.noreply.github.com>
2020-10-19 13:07:04 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,12 +1,39 @@
-(function($){
+(function ($) {
     sct = $(this).scrollTop();
     // h1 클릭시 이벤트
     $(".main_s1_p1 > h1 > a").on("click", function () {
-        for (var i = 1; i < 52; i++) {
-          $(".main_s1_p2").animate({ width: "-=" + 1 + "%" }, 1);
-          $(".main_s1_p1").animate({ width: "+=" + 1 + "%" }, 1);
-        }
-      });
+        // for (var i = 1; i < 52; i++) {
+        //   $(".main_s1_p2").animate({ width: "-=" + 1 + "%" }, 1);
+        //   $(".main_s1_p1").animate({ width: "+=" + 1 + "%" }, 1);
+        // }
+
+        // var flag=true
+        // $(".main_s1_p1 > h1").on("click", function () {
+        //     if ( flag ) {
+        //         for (var i = 1; i < 52; i++) {
+        //             $(".main_s1_p2").animate({ width: "-=" + 1 + "%" }, 1);
+        //             $(".main_s1_p1").animate({ width: "+=" + 1 + "%" }, 1);
+        //           }
+        //         flag = false
+        //     } else if (!flag) {
+        //         for (var i = 52; i < 1; i--) {
+        //             $(".main_s1_p1").animate({ width: "-=" + 1 + "%" }, 1);
+        //             $(".main_s1_p2").animate({ width: "+=" + 1 + "%" }, 1);
+        //         }
+        //         flag=true
+        //     }
+        // });
+
+        // $('.main_s1_p1').animate({
+        //     width:'100%'
+        // },500, function(){
+        //     $('.main_s1_p2').css({
+        //         width:'0%'
+        //     })
+        // })
+        
+
+    });
 
     // 마우스 휠
     $('.section').on('mousewheel', function(e, wh){
